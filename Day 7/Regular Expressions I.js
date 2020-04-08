c570580c76efb7cd22c5438862a3a9aff82ffea1
Soundarya0 <53481890+Soundarya0@users.noreply.github.com>
2020-04-09 01:06:09 +0530

    Add files via upload

---
+++ Day 7/Regular Expressions I.js
@@ -0,0 +1,21 @@
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match a string that starts and ends with the same vowel (i.e., {a, e, i, o, u})
+     */
+    
+    let re = /^(aeiou).*\1$/;
+
+    //let re = /^(a|e|i|o|u).*\1$/;
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
+
+function main() {
+    const re = regexVar();
+    const s = readLine();
+    
+    console.log(re.test(s));
+}
---
+++ Day 7/Regular Expressions III.js
@@ -0,0 +1,24 @@
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match ALL occurrences of numbers in a string.
+     */
+    
+    var re = RegExp('\\d+', 'g');
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
+
+
+function main() {
+    const re = regexVar();
+    const s = readLine();
+    
+    const r = s.match(re);
+    
+    for (const e of r) {
+        console.log(e);
+    }
+}
