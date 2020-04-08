eb12090e08d1bc19ceedca1ac3d7e7b7addd2033
Soundarya0 <53481890+Soundarya0@users.noreply.github.com>
2020-04-09 01:02:10 +0530

    Add files via upload

---
+++ Day 4/Count Objects.js
@@ -0,0 +1,23 @@
+/*
+ * Return a count of the total number of objects 'o' satisfying o.x == o.y.
+ * 
+ * Parameter(s):
+ * objects: an array of objects with integer properties 'x' and 'y'
+ */
+function getCount(objects) {
+    return objects.filter(item => item.x === item.y).length;
+}
+
+
+function main() {
+    const n = +(readLine());
+    let objects = [];
+    
+    for (let i = 0; i < n; i++) {
+        const [a, b] = readLine().split(' ');
+        
+        objects.push({x: +(a), y: +(b)});
+    }
+    
+    console.log(getCount(objects));
+}
