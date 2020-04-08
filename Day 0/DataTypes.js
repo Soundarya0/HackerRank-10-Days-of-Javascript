eef07e3824b470c107a5503acfce5adea56844b8
Soundarya0 <53481890+Soundarya0@users.noreply.github.com>
2020-04-09 01:00:12 +0530

    Add files via upload

---
+++ Day 0/DataTypes.js
@@ -0,0 +1,52 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   The variables 'firstInteger', 'firstDecimal', and 'firstString' are declared for you -- do not modify them.
+*   Print three lines:
+*   1. The sum of 'firstInteger' and the Number representation of 'secondInteger'.
+*   2. The sum of 'firstDecimal' and the Number representation of 'secondDecimal'.
+*   3. The concatenation of 'firstString' and 'secondString' ('firstString' must be first).
+*
+*	Parameter(s):
+*   secondInteger - The string representation of an integer.
+*   secondDecimal - The string representation of a floating-point number.
+*   secondString - A string consisting of one or more space-separated words.
+**/
+function performOperation(secondInteger, secondDecimal, secondString) {
+    // Declare a variable named 'firstInteger' and initialize with integer value 4.
+    const firstInteger = 4;
+    secondInteger = parseInt(secondInteger);
+    console.log(firstInteger + secondInteger);
+    
+    const firstDecimal = 4.0;
+    secondDecimal = parseFloat(secondDecimal);
+    console.log(firstDecimal + secondDecimal);
+   
+    const firstString = 'HackerRank ';
+    console.log(firstString + secondString);
+ 
+    
+}
+
